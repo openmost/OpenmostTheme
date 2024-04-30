d48f888967ce042d3a147a222c842595b34d9962
Ronan HELLO <ronan56.hello@gmail.com>
2024-04-30 18:29:12 +0200

    setup: Theme

--- OpenmostTheme.php
+++ OpenmostTheme.php
@@ -22,6 +22,35 @@ class OpenmostTheme extends Plugin
 
     public function configureThemeVariables(Plugin\ThemeStyles $vars)
     {
-        $vars->fontFamilyBase = 'Verdana, sans-serif';
+        $vars->fontFamilyBase = 'Sora, sans-serif';
+        $vars->colorBrand = '#fff'; // Sort indicator and datatable options
+        $vars->colorBrandContrast = '#ffffff';
+        $vars->colorText = '#AEAFBB';
+        #$vars->colorTextLight = '#FF0000';
+        #$vars->colorTextLighter = '#FF0000';
+        #$vars->colorTextContrast = '#FF0000';
+        #$vars->colorLink = '#FF0000';
+        #$vars->colorBaseSeries = '#FF0000';
+        #$vars->colorHeadlineAlternative = '#FF0000';
+        $vars->colorHeaderBackground = '#1C1F41';
+        #$vars->colorHeaderText = '#FF0000';
+        $vars->colorMenuContrastText = '#AEAFBB';
+        $vars->colorMenuContrastTextSelected = '#ffffff';
+        $vars->colorMenuContrastTextActive = '#ffffff';
+        $vars->colorMenuContrastBackground = '#1C1F41';
+        #$vars->colorWidgetExportedBackgroundBase = '';
+        $vars->colorWidgetTitleText = '#ffffff';
+        $vars->colorWidgetTitleBackground = '#1C1F41';
+        $vars->colorWidgetBackground = '#1C1F41';
+        $vars->colorWidgetBorder = '#1C1F41';
+        $vars->colorBackgroundBase = '#161830'; #161830';
+        $vars->colorBackgroundTinyContrast = '#FF0000';
+        $vars->colorBackgroundLowContrast = '#00FF00';
+        $vars->colorBackgroundContrast = '#1C1F41'; //Table cell color
+        $vars->colorBackgroundHighContrast = '#0000FF';
+        #$vars->colorBorder = '#FF0000';
+        #$vars->colorCode = '#FF0000';
+        $vars->colorCodeBackground = '#FF0000';
+
     }
 }
